a4c8450d1e53c52e27b9d37c7d686fc094bf30ef
Lichen <161976633+Lichen13@users.noreply.github.com>
2024-07-23 18:22:27 -0400

    Update Main.java
    [ Methods Added ]
    displayBio() => Display the biography. (Not written yet)
    startCalculator() => Calculate a chain of operations. (Beta)
    
    [ Modified Methods ]
    displayMenu() => The 2 new methods have been added to the menu.
    displayError() => Some new errors added to the error handler.

--- Main.java
+++ Main.java
@@ -12,6 +12,10 @@ public class Main {
             case "threadError":
                 System.out.println("\nERROR - threadError: Thread stopped. Cannot continue.");
                 return;
+
+            case "lineOutOfBound":
+                System.out.println("\nERROR - lineOutOfBound: The line must contain 3 arguments.");
+                return;
         }
         // WILL BE THE ERROR HANDLER
     }
@@ -38,6 +42,12 @@ public class Main {
         } else if (optionTaken.equals("help")) {
             displayHelp();
 
+        } else if (optionTaken.equals("bio")) {
+            displayBio();
+
+        } else if (optionTaken.equals("start")) {
+            startCalculator();
+
         } else {
             try {
                 displayError("badOption");
@@ -52,8 +62,124 @@ public class Main {
     }
 
 
-    public static void exitSystem() {
+    public static void startCalculator() {
+        int firstArg = 0;
+        int thirdArg = 0;
+        String secondArg = "";
+
+        String startMessage = """
+        \n==={R E A D Y  T O  C A L C U L A T E}===
+        IMPORTANT: - If it is your first time, please read the 'help' section.
+                   - Spaces are required between arguments.
+                   - Maximum and minimum of 3 arguments are required.""";
         
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(startMessage);
+        System.out.print("\n[Line Field] : ");
+
+        String line = scanner.nextLine();
+
+        String[] lineParts = line.split(" ");
+
+        if (lineParts.length == 3) {
+            try {
+                firstArg = Integer.parseInt(lineParts[0]);
+
+            } catch (NumberFormatException e1) {
+                try {
+                    System.out.println("\nERROR - syntaxError: First argument must be a number (Integer).");
+                    Thread.sleep(3000);
+                
+                } catch (InterruptedException e2) {
+                    displayError("threadError");
+                    System.exit(1);
+
+                }
+            }
+
+            try {
+                int isSecArgInt = Integer.parseInt(lineParts[1]);
+
+            } catch (NumberFormatException e1) {
+                switch (lineParts[1]) {
+                    case "+":
+                        secondArg = "+";
+                        break;
+                    
+                    case "-":
+                        secondArg = "-";
+                        break;
+
+                    case "*":
+                        secondArg = "*";
+                        break;
+
+                    case "/":
+                        secondArg = "/";
+
+                    default:
+                        try {
+                            System.out.println("\nERROR - syntaxError: Second argument must be an operation sign ('+', '-', '*', '/').");
+                            Thread.sleep(3000);
+
+                        } catch (InterruptedException e2) {
+                            displayError("threadError");
+
+                        }
+                        break;
+                } 
+            }
+
+            try {
+                thirdArg = Integer.parseInt(lineParts[2]);
+
+            } catch (NumberFormatException e1) {
+                try {
+                    System.out.println("\nERROR - syntaxError: Third argument must be a number (Integer).");
+                    Thread.sleep(3000);
+                
+                } catch (InterruptedException e2) {
+                    displayError("threadError");
+                    System.exit(1);
+
+                }
+            }
+
+            switch (secondArg) {
+                case "+":
+                    System.out.println("\nAnswer is : " + (firstArg + thirdArg));
+                    break;
+
+                case "-":
+                    System.out.println("\nAnswer is : " + (firstArg - thirdArg));
+                    break;
+
+                case "*":
+                    System.out.println("\nAnswer is : " + (firstArg * thirdArg));
+                    break;
+
+                case "/":
+                    System.out.println("\nAnswer is : " + (firstArg / thirdArg));
+                    break;
+            }
+
+        } else {
+            try {
+                displayError("lineOutOfBound");
+                Thread.sleep(3000);
+
+            } catch (InterruptedException e1) {
+                displayError("threadError");
+                System.exit(1);
+
+            }
+        }
+    }
+
+
+    public static void exitSystem() {
         System.out.println("\nExiting system with ERROR STATUS 0. Have a good day.\n");
         System.exit(0);
     }
@@ -106,7 +232,6 @@ public class Main {
                     System.exit(1);
 
                 }
-
             }
         }
 
@@ -115,8 +240,34 @@ public class Main {
 
 
     public static void displayBio() {
+        String bioMessage = """
+        \n==={B I O G R A P H Y}===
+        [!]: Oobs, seems like the biography is empty for now...
+        Type 'back' to come back to menu.""";
 
-        // WILL DISPLAY THE BIOGRAPHY
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println(bioMessage);
+            System.out.print("[?] : ");
+
+            String optionTaken = scanner.next();
+
+            if (optionTaken.equals("back")) {
+                return;
+
+            } else {
+                try {
+                    displayError("badOption");
+                    Thread.sleep(3000);
+                    
+                } catch (InterruptedException e1) {
+                    displayError("threadError");
+                    System.exit(1);
+
+                }
+            }
+        }
     }
     
     
