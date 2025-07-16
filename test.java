67e6f7c04256ab75640320700fd088baf5e752fc
Lichen13 <161976633+lichen13@users.noreply.github.com>
2025-07-16 15:13:02 -0400

    Update test.java
    Creating function: displayMenu

--- test.java
+++ test.java
@@ -1,1 +1,10 @@
-
+public class Main {
+    public static void displayMenu() {
+        String menu = """
+        \n==={M E N U}===
+        exit ............... Exit the program.
+        bio ................ Display the biography.
+        help ............... Display help.
+        start .............. Start the calculator.""";
+        system.out.println(menu)
+}
